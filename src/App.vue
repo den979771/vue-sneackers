35ff70a78fba16ba861f2e541689e445b988a96e
den979771 <den979771@gmail.com>
2024-08-11 12:58:47 +0300

    2 commit

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,15 @@
 		<myHeader/>
 <!--		<Drawer/> -->
 		<div class="p-10">
-			<div class="flex justify-between">
+			<div class="flex justify-between items-center">
 				<h3 class="mb-8 text-3xl text-bold">Все кроссовки</h3>
-				<select>
-					<option>По названию</option>
-					<option>По цене (дешевые)</option>
-					<option>По цене (дорогие)</option>
-				</select>
+				<div class="flex gap-4">
+					<select class="py-2 px-3 border rounded-md outline-none">
+						<option>По названию</option>
+						<option>По цене (дешевые)</option>
+						<option>По цене (дорогие)</option>
+					</select>
+				</div>
 				<div class="relative">
 					<img class="absolute top-3 left-4" src="/search.svg" alt="Search"/>
 					<input class="border rounded-md border-gray-200 py-2 pl-12 pr-4 outline-none focus:border-gray-400" placeholder="Поиск..." />
