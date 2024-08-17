9aa2d493ea48a0e8e8f15fc69b15f9da77116223
den979771 <den979771@gmail.com>
2024-08-17 23:27:55 +0300

    add animations

--- src/components/CartItemList.vue
+++ src/components/CartItemList.vue
@@ -1,12 +1,13 @@
 <script setup>
 	import {inject} from "vue";
 	import CartItem from "./CartItem.vue";
+	
 
 	const {cart, removeFromCart} = inject("cart");
 
 </script>
 <template>
-	<div class="flex flex-col gap-5">
+	<div v-auto-animate class="flex flex-col gap-5">
 		<CartItem 
 			v-for="item in cart" 
 			:key="item.id"
